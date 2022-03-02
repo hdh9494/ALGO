1b87872721e5f26dc7395d5f842704906cf8c380
hdh <gksehdgh94@gmail.com>
2022-03-02 20:51:08 +0900

    HashMap

--- Programmers/LEVEL_2/L2_002.java
+++ Programmers/LEVEL_2/L2_002.java
@@ -4,6 +4,13 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+//  오픈채팅방
+//  input    : String[]
+//  output   : String[]
+//  solution : ["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"]
+//          -> ["Prodo님이 들어왔습니다.", "Ryan님이 들어왔습니다.", "Prodo님이 나갔습니다.", "Prodo님이 들어왔습니다."]
+
+//  https://programmers.co.kr/learn/courses/30/lessons/42888
 public class L2_002 {
     public static void main(String args[]) {
         String[] record = {"Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"};
