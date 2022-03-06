381199f816a4c59be646977a2023bfed9ceefccc
hdh <gksehdgh94@gmail.com>
2022-03-06 23:57:39 +0900

    배열 테스트

---
+++ Programmers/test.java
@@ -0,0 +1,18 @@
+package Programmers;
+
+public class test {
+    public static void main(String[] args) {
+        int[][] arr = new int[3][3];
+
+        arr[0][2] = 1;
+
+        for(int i = 0 ; i < 3 ; i++) {
+            for(int j = 0 ; j < 3; j++) {
+                System.out.print(arr[i][j]);
+            }
+            System.out.println("");
+        }
+
+        
+    }
+}
